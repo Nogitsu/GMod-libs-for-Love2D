92511442dd27d5982c2230f5b75c57d790dec447
Nogitsu <nogitsu.9@gmail.com>
2019-11-16 19:39:19 +0100

    Update surface.lua

--- surface.lua
+++ surface.lua
@@ -73,10 +73,6 @@ function surface.DrawRect( x, y, w, h )
   love.graphics.rectangle( "fill", x, y, w, h )
 end
 
-function surface.DrawPixel( x, y )
-  surface.DrawRect( x, y, 1, 1 )
-end
-
 function surface.DrawOutlinedRect( x, y, w, h )
   love.graphics.rectangle( "line", x, y, w, h )
 end
@@ -86,30 +82,19 @@ function surface.DrawPoly( ... )
   love.graphics.polygon( "fill", ... )
 end
 
-function surface.DrawCircle( x, y, radius, angle_start, angle_end )
+function surface.DrawCircle( x, y, radius )
     local poly = {}
     table.insert( poly, { x = x, y = y } )
 
-    for i = math.min( angle_start or 0, angle_end or 360 ), math.max( angle_start or 0, angle_end or 360 ) do
+    for i = 0, 360 do
         local a = math.rad( i )
-        if ( angle_start or 0 ) < 0 then
-            table.insert( poly, { x = x + math.cos( a ) * radius, y = y + math.sin( a ) * radius } )
-        else
-            table.insert( poly, { x = x - math.cos( a ) * radius, y = y - math.sin( a ) * radius } )
-        end
+        table.insert( poly, { x = x - math.cos( a ) * radius, y = y - math.sin( a ) * radius } )
     end
     table.insert( poly, { x = x, y = y } )
 
     surface.DrawPoly( poly )
 
     return poly
-end
-
-function surface.DrawElipse( x, y, w, h )
-    surface.DrawCircle( x + h / 2, y + h / 2 + (y == 0 and 0 or - .5), h / 2, 90, -90 )
-    surface.DrawCircle( x + w - h / 2, y + h / 2 + (y == 0 and 0 or - .5), h / 2, -90, 90 )
-
-    surface.DrawRect( x + h / 2, y - 1, w - h + 2, h + 1 )
 end
 
 --  > Fonts
@@ -154,34 +139,4 @@ function surface.GetTextSize( text )
   templab:set( text )
 
   return templab:getDimensions()
-end
-
---  > Custom
-function Lerp( delta, from, to )
-
-	if ( delta > 1 ) then return to end
-	if ( delta < 0 ) then return from end
-
-	return from + ( to - from ) * delta
-
-end
-
-function LerpColor( t, c1, c2 )
-    return Color( Lerp( t, c1.r, c2.r ), Lerp( t, c1.g, c2.g ), Lerp( t, c1.b, c2.b ), c1.a == c2.a and c1.a or Lerp( t, c1.a, c2.a ) )
-end
-
-function surface.DrawGradient( x, y, w, h, color1, color2, vertical )
-    vertical = vertical or false
-
-    if not vertical then
-        for i = 0, w do
-            surface.SetDrawColor( LerpColor( i / w, color1, color2 ) )
-            surface.DrawLine( x + i, y, x + i, y + h )
-        end
-    else
-        for i = 0, h do
-            surface.SetDrawColor( LerpColor( i / h, color1, color2 ) )
-            surface.DrawLine( x, y + i, x + w, y + i )
-        end
-    end
 end
